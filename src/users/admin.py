a87a5276cb20295ed797c8427c51e7e915632440
Wassim Maaoui <wassimmaaoui02@gmail.com>
2024-09-07 22:35:59 +0100

    automax part 2

--- src/users/admin.py
+++ src/users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Location, Profile
 
 # Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+class LocationAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Location, LocationAdmin)
--- src/users/models.py
+++ src/users/models.py
@@ -1,3 +1,26 @@
+from email.policy import default
 from django.db import models
+from django.contrib.auth.models import User
+from localflavor.us.models import USStateField, USZipCodeField
 
 # Create your models here.
+class Location(models.Model):
+    address_1 = models.CharField(max_length=128, blank=True)
+    address_2 = models.CharField(max_length=128, blank=True)
+    city = models.CharField(max_length=64)
+    state = USStateField(default="NY")
+    zid_code = USZipCodeField(blank=True)
+
+    def __str__(self) -> str:
+        return f"Location {self.id}"
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE)
+    photo = models.ImageField(null=True)
+    bio = models.CharField(max_length=140,blank=True)
+    phone_number = models.CharField(max_length=12,blank=True)
+    location = models.OneToOneField('users.Location', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self) -> str:
+        return f"{self.user.username}'s profile".capitalize()
